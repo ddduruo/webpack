86e78dde4b819bceabdfb5b808b3fa271d40fd67
andy00614 <33732843@qq.com>
2020-05-26 17:58:46 +0800

    fix: filename has more space

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const path = require('path')
 module.exports = {
+  mode: "development",
   entry:'./src/index.js',
   output:{
     path: path.resolve(__dirname,'dist'), //动态获取路径
-    filename: 'bundle.js '
+    filename: 'bundle.js'
   },
 }
